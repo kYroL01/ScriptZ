8caf3d79dbbd1d9cc0cc076942fed2a3de7eb48e
kyrol <fci1908@gmail.com>
2020-02-04 10:57:00 +0100

    ADD: delete repository bash script from Debian/Ubuntu or Centos system

---
+++ del_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+### Michele Campus - michelecampus5@gmail.com ###
+## The script take a repository name as Input and delete it from the existing list under /etc/apt/sources.list.d/ ##
+
+DEL_REPO="$1"
+OS="$2"
+
+if [[ "$OS" == "debian" || "$OS" == "ubuntu" ]]
+then # Debian - Ubuntu
+	if [ -f /etc/apt/sources.list.d/"${DEL_REPO}" ]
+	then
+		sudo rm /etc/apt/sources.list.d/"${DEL_REPO}"
+	else
+		echo "!!! Repository ${DEL_REPO} in ${OS} NOT FOUND !!!"
+		exit 1
+	fi
+else # Centos
+	if [ -f /etc/yum.repos.d/"${DEL_REPO}" ]
+	then
+		sudo rm /etc/yum.repos.d/"${DEL_REPO}"
+        else
+                echo "!!! Repository ${DEL_REPO} in ${OS} NOT FOUND !!!"
+                exit 1
+        fi
+fi
+
+echo "--- Repository ${DEL_REPO} deleted successfully in ${OS} ---"
