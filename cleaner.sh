c8db516faa200fbedb3ab2cccdeacbacf824d02c
kyrol <fci1908@gmail.com>
2020-04-22 15:33:42 +0200

    ADD: new docker cleanup

--- cleaner.sh
+++ cleaner.sh
@@ -26,19 +26,19 @@ sudo rm -rf ~/.thumbnails/*
 printf "DELETING THUMBNAIL			...[OK]\n" 
 
 
-# Browser cache
-printf "<< Deleting browsers cache >>\n" 
-
-sudo rm -rf ~/.mozilla/firefox/*.default/Cache/* #Firefox/Iceweasel
-printf "DELETING FIREFOX/ICEWEASEL CACHE	...[OK]\n" 
-sudo rm -rf ~/.cache/google-chrome/Default/Cache/* #Google Chrome
-printf "DELETING GOOGLE CHROME CACHE		...[OK]\n" 
-
 
 # Local Cache
 printf "<< Deleting local cache >>\n" 
 sudo rm -rf ~/.cache/*
 printf "DELETING LOCAL CACHE FILES		...[OK]\n" 
+
+# Docker cleaning
+printf "<< Deleting docker containers >>\n" 
+sudo docker container prune
+printf "DELETING DOCKER CONTAINER		...[OK]\n" 
+printf "<< Deleting docker images >>\n" 
+sudo docker image prune -a
+printf "DELETING DOCKER IMAGES			...[OK]\n" 
 
 
 printf "<<<--- Clean completed. Juve merda!! --->>>\n"
