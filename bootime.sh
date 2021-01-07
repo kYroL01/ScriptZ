7e7b5fc38cb539af888acdc7ef990cd3d88e9614
kyrol <fci1908@gmail.com>
2021-01-07 16:07:14 +0100

    ADD: bootime.sh

---
+++ bootime.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# bash script to shhow system reboot and shutdown date and time
+#
+echo "--------------------------------------------"
+echo "--- time of last system boot ---"
+who -b
+echo
+echo "--- last reboot ---"
+last reboot
+echo
+echo "--- last shutdown ---"
+last shutdown
+echo
+echo "--- how long the system has been running ---"
+uptime
+cat /proc/uptime
+awk '{ print "up " $1 /60 " minutes"}' /proc/uptime 
+w
+echo "--------------------------------------------"
