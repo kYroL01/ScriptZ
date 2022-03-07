b449e574b9533e15c135114c15081ef6c3fe4294
kyrol <fci1908@gmail.com>
2022-03-07 18:12:03 +0100

    ADD: added new script mem_test_app.sh - fix minor comment

---
+++ mem_test_app.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# author: Michele Campus
+#
+# bash script to test memory consumption of an application
+# it prints result in test_mem.log
+#
+
+read -p "New test ? [yes/no] " new
+if [ "$new" == "yes" ]; then
+	rm -rf test_mem.log
+fi
+echo "" >> test_mem.log
+read -p "Number of test: " num_test
+echo "$num_test" >> test_mem.log
+read -p "Description: " descr
+echo "$descr" >> test_mem.log
+read -p "Program name: " progr
+
+# free mem command
+free -h >> test_mem.log
+
+if [ "$num_test" -gt 0 ]; then
+	cat /proc/$(pgrep "$progr")/status | grep -i Threads >> test_mem.log
+fi
+
+if [ "$?" -gt 0 ]; then
+	rm test_mem.log
+fi
+
